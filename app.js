3fc43722fae13e281b56495086d68173bdf54263
unknown <mryanmarcellino@gmail.com>
2023-02-03 01:45:10 +0700

    feat: update config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let app = express()
-let port = 3000
+let port = process.env.PORT || 3000
 let models = require('./models/index')
 let appRoute = require('./app/routes/index')
 
